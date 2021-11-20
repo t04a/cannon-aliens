9c880abd0cbec145cd1ba536ce384cd40aba6ae1
Roman <roman@dev.com>
2021-11-20 21:28:34 +0300

    component Canvas

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,21 @@
+import Sky from "./Sky";
+
+function Canvas(props) {
+    const viewBox = [
+        window.innerWidth / -2,
+        100 - window.innerHeight,
+        window.innerWidth,
+        window.innerHeight];
+    return (
+        <svg
+            id="aliens-go-home-canvas"
+            preserveAspectRatio="xMaxYMax none"
+            viewBox={viewBox}
+        >
+            <Sky/>
+            <circle cx={0} cy={0} r={50}/>
+        </svg>
+    );
+}
+
+export default Canvas;
