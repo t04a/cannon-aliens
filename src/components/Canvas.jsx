6a7b7b2f459301a19687e3507bafd4b0cf449cc0
Roman <roman@dev.com>
2021-11-22 18:53:31 +0300

    add gameStart, gameState to Canvas.jsx.js

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -29,20 +29,34 @@ function Canvas(props) {
             <Ground/>
             <CannonPipe rotation={props.angle}/>
             <CannonBase/>
-            <CannonBall position={{x: 0, y: -100}}/>
             <CurrentScore score={15}/>
-            <FlyingObject position={{x: -150, y: -300}}/>
-            <FlyingObject position={{x: 150, y: -300}}/>
-            <Heart position={{x: -300, y: 35}}/>
-            <StartGame onClick={() => console.log('Aliens, Go Home!')}/>
-            <Title/>
+
+            { ! props.gameState.started &&
+            <g>
+                <StartGame onClick={() => props.startGame()} />
+                <Title />
+            </g>
+            }
+
+            { props.gameState.started &&
+            <g>
+                <FlyingObject position={{x: -150, y: -300}}/>
+                <FlyingObject position={{x: 150, y: -300}}/>
+            </g>
+            }
         </svg>
     );
 }
 
 Canvas.propTypes = {
     angle: PropTypes.number.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+    }).isRequired,
     trackMouse: PropTypes.func.isRequired,
+    startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
