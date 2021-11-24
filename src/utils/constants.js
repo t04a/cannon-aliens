9695fe2d7ea9ef6a31b0f21a8cb35d68218a6a61
Roman <roman@dev.com>
2021-11-24 15:38:30 +0300

    new constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,18 @@
 export const skyAndGroundWidth = 5000;
+
 export const gameWidth = 800;
+
 export const gameHeight = 1200;
+
+export const createInterval = 1000;
+
+export const maxFlyingObjects = 4;
+
+export const flyingObjectsStarterYAxis = -1000;
+
+export const flyingObjectsStarterPositions = [
+    -300,
+    -150,
+    150,
+    300,
+];
