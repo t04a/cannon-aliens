6ce70d60a1e86ca3c890b17650e8e1b10eca45e4
Roman <roman@dev.com>
2021-11-29 17:46:48 +0300

    processing action SHOOT

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
-import {MOVE_OBJECTS, START_GAME} from "../actions";
+import {MOVE_OBJECTS, SHOOT, START_GAME} from "../actions";
 import moveObjects from "./moveObjects";
 import startGame from "./startGame";
+import shoot from "./shoot";
 
 const initialGameState = {
     started: false,
@@ -8,6 +9,7 @@ const initialGameState = {
     lives: 3,
     flyingObjects: [],
     lastObjectCreatedAt: new Date(),
+    cannonBalls: [],
 };
 
 const initialState = {
@@ -20,7 +22,9 @@ function reducer(state = initialState, action) {
         case MOVE_OBJECTS:
             return moveObjects(state, action);
         case START_GAME:
-            return startGame(state, initialGameState)
+            return startGame(state, initialGameState);
+        case SHOOT:
+            return shoot(state, action)
         default:
             return state
     }
